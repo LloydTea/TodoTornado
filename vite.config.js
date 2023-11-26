04a15ecda3820edc7f7016dd2fe7c355277e69de
LloydTea <Lloyd.mft@gmail.com>
2023-11-26 23:58:10 +0400

    Vite Config Update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import sass from "vite-plugin-sass";
+import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: { outDir: "../dist" },
   plugins: [sass(), react()],
   resolve: {
     alias: {
-      src: "/src",
-      node_modules: "/node_modules",
+      // Resolve src to /src directory
+      src: path.resolve(__dirname, "src"),
+      // Resolve node_modules to /node_modules directory
+      node_modules: path.resolve(__dirname, "node_modules"),
     },
   },
 });
