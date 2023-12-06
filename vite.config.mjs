8e1743e40c1e805de3c9ed49f69054b0d0d43f61
LloydTea <Lloyd.mft@gmail.com>
2023-12-06 05:48:39 -0500

    Testing Git Runner

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import sass from "vite-plugin-sass";
 
 export default defineConfig({
   build: { outDir: "./dist" },
